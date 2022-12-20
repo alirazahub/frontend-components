207cb7f33d1648352ee7768cd1003a587c99aad9
alirazahub <aliraza.ar7785@gmail.com>
2022-12-20 06:54:45 +0500

    front-end components

---
+++ toDoList/script.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+    let task = document.querySelector(".input-box");
+    let list = document.querySelector(".line");
+    let addTask = document.querySelector(".btn");
+
+    addTask.addEventListener("click", ()=>{
+        list.insertAdjacentHTML(
+            `afterbegin`,
+            `<li>
+                   <i class="fas fa-trash deleteTask"></i>
+
+                   <h4>${task.value}</h4>
+            </li>`
+        );
+
+        task.value = "";
+
+        let deleteTask = document.querySelectorAll(".deleteTask");
+
+        deleteTask.forEach((del) =>{
+            del.addEventListener("click", ()=>{
+                del.parentElement.remove();
+
+            });
+        });
+    });
+};
